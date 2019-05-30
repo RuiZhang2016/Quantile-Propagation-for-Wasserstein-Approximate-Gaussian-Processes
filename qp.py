ed48efdd687a1eaa2871ffa47579b5843482381a
Rui Zhang <ruizhang@Ruis-MacBook-Pro.local>
2019-05-31 09:31:16 +1000

    update

--- qp.py
+++ qp.py
@@ -6,7 +6,7 @@ import time
 def Fr(x,m,v,mu,sigma):
     Z = norm.cdf((mu-m)/v/np.sqrt(1+sigma**2/v**2))
     A = 1/Z
-    k = (mu-m)/v/np.sqrt(sigma**2/v**2+1)
+    k = (mu-m)/np.sqrt(sigma**2+v**2)
     h = (x-mu)/sigma
     rho = 1/np.sqrt(1+v**2/sigma**2)
     # print('Z: {}, \nA: {}, \nk: {}, \nh: {}, \nrho: {}'.format(Z,A,k,h,rho))
@@ -16,13 +16,19 @@ def Fr(x,m,v,mu,sigma):
     assert k != 0, 'k is 0'
     OT1 = owens_t(h,(k+rho*h)/h/np.sqrt(1-rho**2))
     OT2 = owens_t(k,(h+rho*k)/k/np.sqrt(1-rho**2))
-    return A*(0.5*norm.cdf(h)+0.5*norm.cdf(k)-OT1-OT2+eta)
-
+    if v>0:
+        return A*(0.5*norm.cdf(h)+0.5*norm.cdf(k)-OT1-OT2+eta)
+    if v<0:
+        return A*norm.cdf(h)-A * (0.5 * norm.cdf(h) + 0.5 * norm.cdf(k) - OT1 - OT2 + eta)
 
 def Fr_MC(x,m,v,mu,sigma):
     Z = norm.cdf((mu - m) / v / np.sqrt(1 + sigma ** 2 / v ** 2))
-    samples = np.linspace(-100,x,100000)
+    samples = np.linspace(-50,x,100000)
     values = norm.cdf((samples-m)/v)*norm.pdf(samples, loc=mu,scale = sigma)
     d = samples[1]-samples[0]
-    integral = np.sum(values[:-1]+values[1:])/2*(samples[1]-samples[0])
+    integral = np.sum(values[:-1]+values[1:])/2*d
     return integral/Z
+
+
+def integral_Fr():
+    pass
