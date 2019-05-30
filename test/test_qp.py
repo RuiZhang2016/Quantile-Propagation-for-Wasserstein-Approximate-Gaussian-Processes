a2d0a841c9f10590fffec4e9dbcdfbc55c49bead
Rui Zhang <ruizhang@Ruis-MacBook-Pro.local>
2019-05-31 09:31:16 +1000

    update

--- test/test_qp.py
+++ test/test_qp.py
@@ -6,12 +6,16 @@ import numpy as np
 class TestQp(TestCase):
 
     def test_Fr(self):
-        for x in np.linspace(1,100,50):
-            for m in np.linspace(1,2,5):
-                t = time.time()
-                res1 = qp.Fr(x, m, 1, 0, 1)
-                print(time.time() - t, ' seconds')
-                t = time.time()
-                res2 = qp.Fr_MC(x, m, 1, 0, 1)
-                print(time.time() - t, ' seconds')
-                assert np.isclose(res1,res2),(res1,res2)
+        ms = [-2,-1,1,2]
+        vs = [-2,-1,1,2]
+        for x in np.linspace(-10,10,500):
+            for m in ms:
+                for v in vs:
+                    # t = time.time()
+                    res1 = qp.Fr(x, m, v, 0, 1)
+                    # print(time.time() - t, ' seconds')
+                    # t = time.time()
+                    res2 = qp.Fr_MC(x, m, v, 0, 1)
+                    # print(time.time() - t, ' seconds')
+                    assert np.isclose(res1,res2),(x,' Fr, Fr_MC',res1,res2)
+                    print(res1,res2)
