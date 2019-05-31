d6235a938f87bfa117684653596b46807affc0aa
Rui Zhang <u59634369@anu.edu.au>
2019-05-31 19:23:41 +1000

    add Fr and inverse Fr

--- qp.py
+++ qp.py
@@ -1,7 +1,11 @@
 import numpy as np
 from scipy.stats import norm
 from scipy.special import owens_t
+import scipy.integrate as integrate
 import time
+from scipy.special import erfinv
+from pynverse import inversefunc
+import matplotlib.pyplot as plt
 
 def Fr(x,m,v,mu,sigma):
     Z = norm.cdf((mu-m)/v/np.sqrt(1+sigma**2/v**2))
@@ -11,15 +15,29 @@ def Fr(x,m,v,mu,sigma):
     rho = 1/np.sqrt(1+v**2/sigma**2)
     # print('Z: {}, \nA: {}, \nk: {}, \nh: {}, \nrho: {}'.format(Z,A,k,h,rho))
     eta = 0 if h*k>0 or (h*k==0 and h+k >= 0) else -0.5
-    # print('cdf_h: {}, \ncdf_k: {}, \nowt(h,k): {}, \nowt(k,h): {}\neta: {}'.format(norm.cdf(h),norm.cdf(k),owens_t(h,(k-rho*h)/h/np.sqrt(1-rho**2)),owens_t(k,(h-rho*k)/k/np.sqrt(1-rho**2)),eta))
-    assert h != 0, 'h is 0'
-    assert k != 0, 'k is 0'
-    OT1 = owens_t(h,(k+rho*h)/h/np.sqrt(1-rho**2))
-    OT2 = owens_t(k,(h+rho*k)/k/np.sqrt(1-rho**2))
+    if h ==0 and k == 0:
+        return A*(0.25+1/np.sin(-rho))
+    # OT1 = owens_t(h,(k+rho*h)/h/np.sqrt(1-rho**2))
+    # OT2 = owens_t(k,(h+rho*k)/k/np.sqrt(1-rho**2))
+    OT1 = my_owens_t(h,k,rho)
+    OT2 = my_owens_t(k,h,rho)
     if v>0:
         return A*(0.5*norm.cdf(h)+0.5*norm.cdf(k)-OT1-OT2+eta)
     if v<0:
-        return A*norm.cdf(h)-A * (0.5 * norm.cdf(h) + 0.5 * norm.cdf(k) - OT1 - OT2 + eta)
+        return A * (0.5 * norm.cdf(h) - 0.5 * norm.cdf(k) + OT1 + OT2 - eta)
+
+def pr(x,m,v,mu,sigma):
+    Z = norm.cdf((mu - m) / v / np.sqrt(1 + sigma ** 2 / v ** 2))
+    return norm.cdf((x-m)/v)*norm.pdf(x,loc=mu,scale=sigma)/Z
+
+
+def my_owens_t(x1,x2,rho):
+    if x1 == 0 and x2>0:
+        return 0.25
+    elif x1 == 0 and x2<0:
+        return -0.25
+    else:
+        return owens_t(x1,(x2+rho*x1)/x1/np.sqrt(1-rho**2))
 
 def Fr_MC(x,m,v,mu,sigma):
     Z = norm.cdf((mu - m) / v / np.sqrt(1 + sigma ** 2 / v ** 2))
@@ -29,6 +47,44 @@ def Fr_MC(x,m,v,mu,sigma):
     integral = np.sum(values[:-1]+values[1:])/2*d
     return integral/Z
 
+def fit_gauss_wd(m,v,mu,sigma):
+    z = (mu-m)/v/np.sqrt(1+sigma**2/v**2)
+    inf_mu = mu+sigma**2*norm.pdf(z)/norm.cdf(z)/v/np.sqrt(1+sigma**2/v**2)
+    inverse_Fr = lambda y: inversefunc(lambda x: Fr(x,m,v,mu,sigma),y_values=y)
+    C2 = np.sqrt(2)*integrate.quad(lambda x: erfinv(2*x-1)*inverse_Fr(x),0,1)[0]
+    inf_sigma = C2
+    return inf_mu,inf_sigma
 
-def integral_Fr():
-    pass
+def fit_gauss_kl(m,v,mu,sigma):
+    sigma2 = sigma**2
+    v2 = v**2
+    z = (mu-m)/v/np.sqrt(1+sigma2/v2)
+    inf_mu = mu+sigma2*norm.pdf(z)/norm.cdf(z)/v/np.sqrt(1+sigma2/v2)
+    inf_sigma2 = sigma2-sigma2**2*norm.pdf(z)/(v2+sigma2)/norm.cdf(z)*(z+norm.pdf(z)/norm.cdf(z))
+    return inf_mu,np.sqrt(inf_sigma2)
+
+if __name__ == '__main__':
+    m,v,mu,sigma = 1,2,3,4
+    inf_mu_wd,inf_sigma_wd = fit_gauss_wd(m,v,mu,sigma)
+    inf_mu_kl, inf_sigma_kl = fit_gauss_kl(m, v, mu, sigma)
+    print('wd: mu ',inf_mu_wd,' sigma:',inf_sigma_wd)
+    print('kl: mu ', inf_mu_kl, ' sigma:', inf_sigma_kl)
+    xplot = np.linspace(-5,5,100)
+    yplot_true = pr(xplot,m,v,mu,sigma)
+    yplot_wd = norm.pdf(xplot,loc=inf_mu_wd,scale=inf_sigma_wd)
+    yplot_kl = norm.pdf(xplot, loc=inf_mu_kl, scale=inf_sigma_kl)
+    plt.plot(xplot,yplot_true,label='True')
+    plt.plot(xplot, yplot_wd, label='wd')
+    plt.plot(xplot, yplot_kl, label='kl')
+    plt.legend()
+    plt.show()
+
+    yplot_true = np.array([Fr(e, m, v, mu, sigma) for e in xplot])
+    yplot_wd = norm.cdf(xplot, loc=inf_mu_wd, scale=inf_sigma_wd)
+    yplot_kl = norm.cdf(xplot, loc=inf_mu_kl, scale=inf_sigma_kl)
+    plt.plot(xplot, yplot_true, label='True')
+    plt.plot(xplot, yplot_wd, label='wd')
+    plt.plot(xplot, yplot_kl, label='kl')
+    plt.legend()
+    plt.show()
+
--- test/test_qp.py
+++ test/test_qp.py
@@ -2,13 +2,14 @@ from unittest import TestCase
 import qp
 import time
 import numpy as np
+from scipy.special import erfinv
+import scipy.integrate as integrate
 
 class TestQp(TestCase):
-
     def test_Fr(self):
         ms = [-2,-1,1,2]
         vs = [-2,-1,1,2]
-        for x in np.linspace(-10,10,500):
+        for x in np.linspace(-10,10,50):
             for m in ms:
                 for v in vs:
                     # t = time.time()
@@ -18,4 +19,33 @@ class TestQp(TestCase):
                     res2 = qp.Fr_MC(x, m, v, 0, 1)
                     # print(time.time() - t, ' seconds')
                     assert np.isclose(res1,res2),(x,' Fr, Fr_MC',res1,res2)
-                    print(res1,res2)
+
+    def test_inverse_Fr(self):
+        print('Test Inverse Func')
+        from pynverse import inversefunc
+        ms = [-2, -1, 1, 2]
+        vs = [-2, -1, 1, 2]
+        for m in ms:
+            for v in vs:
+                func = lambda x: qp.Fr(x, m, v, 0, 1)
+                for x in np.linspace(0,1,10):
+                    assert np.isclose(x,func(inversefunc(func, y_values=x)))
+
+    def test_Fr_vectorisation(self):
+        x = np.linspace(1,10,100)
+        m, v, mu, sigma = 1, 2, 3, 4
+        v = qp.Fr(x,m, v, mu, sigma)
+
+    # def test_fit_gauss_wd(self):
+    #     ms = [-2, -1, 1, 2]
+    #     vs = [-2, -1, 1, 2]
+    #     mu, sigma = 1,1
+    #     for m in ms:
+    #         for v in vs:
+    #             _,C2 = qp.fit_gauss_wd(m, v, mu, sigma)
+    #             f2 = lambda x: np.sqrt(2)*erfinv(2*qp.Fr(x,m,v,mu,sigma)-1)\
+    #                            *x*qp.pr(x,m,v,mu,sigma)
+    #             xs = np.linspace(-10,10,100000)
+    #             ys = np.array([f2(x) for x in xs])*(xs[1]-xs[0])/2
+    #             C2_2 = np.sum(ys[:-1]+ys[1:])
+    #             print(C2,C2_2)
