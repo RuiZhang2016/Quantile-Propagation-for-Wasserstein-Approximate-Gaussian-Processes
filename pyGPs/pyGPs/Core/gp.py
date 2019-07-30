0bd17c71fbf2d18c846531ffcb8baf833401e8ef
Rui Zhang <ruizhang@Ruis-MacBook-Pro.local>
2019-07-30 23:45:25 +1000

    update

--- pyGPs/pyGPs/Core/gp.py
+++ pyGPs/pyGPs/Core/gp.py
@@ -647,7 +647,7 @@ class GPC(GP):
         self.meanfunc = mean.Zero()                        # default prior mean
         self.covfunc = cov.RBF()                           # default prior covariance
         self.likfunc = lik.Erf()                           # erf likihood
-        self.inffunc = inf.QP()                            # default inference method
+        self.inffunc = inf.EP()                            # default inference method
         self.optimizer = opt.Minimize(self)                # default optimizer
 
 
@@ -712,6 +712,8 @@ class GPC(GP):
         '''
         if newInf == "Laplace":
             self.inffunc = inf.Laplace()
+        elif newInf == "QP":
+            self.inffunc = inf.QP()
         else:
             raise Exception('Possible inf values are "Laplace".')
 
