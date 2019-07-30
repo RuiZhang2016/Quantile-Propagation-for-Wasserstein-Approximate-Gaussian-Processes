acab2488a16c90a1debbf9da092aa3b0e3ca76b1
Rui Zhang <u59634369@anu.edu.au>
2019-07-30 21:54:02 +1000

    update

--- core/generate_table.py
+++ core/generate_table.py
@@ -4,6 +4,7 @@ from pynverse import inversefunc
 import scipy.integrate as integrate
 from scipy.special import erfinv
 import csv
+import os
 
 def generate_table(v):
     mu_range = np.linspace(-2,2,401)
@@ -33,22 +34,47 @@ def generate_table(v):
         writer = csv.writer(wf)
         writer.writerows(table)
 
-def read_table(file):
-    with open(file,'r') as rf:
-        reader = csv.reader(rf)
-        lines = list(reader)
-        z = [[float(e) for e in l] for l in lines]
-    return z
+def WR_table(file,op,table=None):
+    if op == 'r':
+        with open(file,'r') as rf:
+            reader = csv.reader(rf)
+            lines = list(reader)
+            z = [[float(e) for e in l] for l in lines]
+            return np.array(z)
+    elif op == 'w':
+        with open(file, 'w') as wf:
+            writer = csv.writer(wf)
+            writer.writerows(table)
+            return True
+    else:
+        raise NotImplementedError
+
+
+def compress(v):
+    table = WR_table('/home/rzhang/PycharmProjects/WGPC/tmp/sigma_-10.00_{}.csv'.format(v),'r')
+    for i in range(2,401):
+        sv = i/20-10.05
+        filename = '/home/rzhang/PycharmProjects/WGPC/tmp/sigma_{:.2f}_{}.csv'.format(sv,v)
+        if os.path.exists(filename):
+            z = WR_table(filename,'r')
+            table = np.vstack((table, z))
+            assert WR_table('../res/WD_GPC/sigma_{}.csv'.format(v),'w',table)
+        else:
+            raise Exception(filename, ' not exists')
+    print(table.shape)
+
 
 
 if __name__ == '__main__':
-    # generate_table(1)
-    table = [[1.0002,2,3],[2.1,3.1,4.1]]
-    with open('sigma_{}.csv'.format(1),'w') as wf:
-        writer = csv.writer(wf)
-        writer.writerows(table)
-
-    with open('sigma_{}.csv'.format(1),'r') as rf:
-        reader = csv.reader(rf)
-        lines = list(reader)
-        print([[float(e) for e in l] for l in lines])
+    compress(-1)
+    # compress(-1)
+    # # generate_table(1)
+    # table = [[1.0002,2,3],[2.1,3.1,4.1]]
+    # with open('sigma_{}.csv'.format(1),'w') as wf:
+    #     writer = csv.writer(wf)
+    #     writer.writerows(table)
+    #
+    # with open('sigma_{}.csv'.format(1),'r') as rf:
+    #     reader = csv.reader(rf)
+    #     lines = list(reader)
+    #     print([[float(e) for e in l] for l in lines])
